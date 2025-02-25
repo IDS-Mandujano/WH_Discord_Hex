841527b1d52b9c944f13af79d7cb55904aa80573
Eduardo Mandujano <jesusgutierrez191105@gmail.com>
2025-02-25 01:58:15 -0600

    webhook github & discord

---
+++ application/discord.go
@@ -0,0 +1,33 @@
+package application
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type DiscordMessage struct {
+	Content string `json:"content"`
+}
+
+func SendDiscordMessage(message string) {
+	godotenv.Load()
+	webhookURL := os.Getenv("DISCORD_wH_URL")
+
+	if webhookURL == "" {
+		log.Println("⚠️ No se encontró la URL del webhook de Discord en .env")
+		return
+	}
+
+	payload := DiscordMessage{Content: message}
+	jsonValue, _ := json.Marshal(payload)
+
+	_, err := http.Post(webhookURL, "application/json", bytes.NewBuffer(jsonValue))
+	if err != nil {
+		log.Println("❌ Error al enviar mensaje a Discord:", err)
+	}
+}
